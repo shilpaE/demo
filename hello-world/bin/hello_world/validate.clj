46a5c04fddc36afd5e0e8d3418de5701ea75f020
shilpaE <37647635+shilpaE@users.noreply.github.com>
2018-03-28 09:54:37 +0530

    Add files via upload

---
+++ hello-world/bin/hello_world/validate.clj
@@ -0,0 +1,13 @@
+(ns vvv
+   (:gen-class) (:require [clojure.data.json :as json])
+  (:require [clojure.java.io :as set]))
+(def a(hash-set  ))
+(def b(hash-set :a :b))
+(try (cond
+       (and (empty? a) (empty? b))(println "Both r empty")
+       (empty? a) ((println b)(println "data in b"))
+       (empty? b)( (println a)(println "data in a"))
+       :else (do(println "data in both a and b")(println a b))
+       )
+     (catch Exception e (println (str " " (.getMessage e)))))
+
